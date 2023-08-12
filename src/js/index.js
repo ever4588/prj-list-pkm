27a8bed2256aa7aa804065983260c2a9d5a054f7
ever4588 <115740065+ever4588@users.noreply.github.com>
2023-08-12 14:42:10 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+
+const body = document.querySelector("body");
+
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao")
+
+
+
+botaoAlterarTema.addEventListener("click", () => {
+
+    const modoEscutoEstaAtivo = body.classList.contains("modo-escuro");
+
+    body.classList.toggle("modo-escuro");
+
+    if (modoEscutoEstaAtivo) {
+        // body.classList.remove("modo-escuro");
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+
+    } else {
+        // body.classList.add("modo-escuro");
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
+    }
+
+
+});
+
